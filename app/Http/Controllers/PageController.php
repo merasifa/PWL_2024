f01cc88edeeee3c90af4eb5a27eafffdaf2d62a7
Tiara Mera S <merasifa@gmail.com>
2025-02-17 10:38:44 +0700

    PRAKTIKUM

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        return 'Selamat Datang';
+    }
+
+    public function about() {
+        return 'Nama: Tiara Mera Sifa <br> NIM: 2341720247';
+    }
+
+    public function articles($id){
+        return 'Halaman Artikel dengan id $id';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+
+Route::get('/about', function () {
+    return '2341720247 TIARA MERA SIFA';
+});
+
+Route::get('/user/{Mera}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke- '.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/', [WelcomeController::class,'hello']);
+Route::get('/about', [WelcomeController::class,'hello']);
+Route::get('/articles/', [WelcomeController::class,'hello']);
